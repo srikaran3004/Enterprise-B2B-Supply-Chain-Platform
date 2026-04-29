1c511ed7faf0bf6d62be8245dcaa95d0a1e5e16f
Srikaran Devarakonda <srikaran2230@gmail.com>
2026-04-29 19:07:34 +0530

    feat: add optional district field to ShippingAddress and AddShippingAddressDto interfaces

--- EnterpriseB2BSupplyChain Frontend/hul-supply-portal/src/app/core/models/shipping-address.model.ts
+++ EnterpriseB2BSupplyChain Frontend/hul-supply-portal/src/app/core/models/shipping-address.model.ts
@@ -2,6 +2,7 @@ export interface ShippingAddress {
   addressId: string;
   label: string;
   addressLine1: string;
+  district?: string;
   city: string;
   state: string;
   pinCode: string;
@@ -12,6 +13,7 @@ export interface ShippingAddress {
 export interface AddShippingAddressDto {
   label: string;
   addressLine1: string;
+  district?: string;
   city: string;
   state: string;
   pinCode: string;
